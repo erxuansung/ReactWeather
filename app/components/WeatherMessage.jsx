64e000f4346aa373dea95b657de037d45e76e1a9
Peter <peter@chengdeMacBook-Air.local>
2017-02-01 16:01:58 +0800

    Added copy

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -9,7 +9,7 @@ var React = require('react');
 //   }
 // });
 
-var WeatherMessage = ({props}) => {
+var WeatherMessage = (props) => {
   var {temp, location} = props;
   return (
     <h3>It's it {temp} in {location}.</h3>
